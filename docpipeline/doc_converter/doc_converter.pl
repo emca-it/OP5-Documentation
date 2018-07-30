b8d0fafeee254b52d10c17b63d4962f456dc2005
Ryan Quinn <rquinn@op5.com>
2018-07-30 07:10:04 -0500

    WIP checkpoint.
     On branch doc_converter_script
     Changes to be committed:
    	new file:   default.doclist
    	modified:   doc_converter.pl

--- docpipeline/doc_converter/doc_converter.pl
+++ docpipeline/doc_converter/doc_converter.pl
@@ -24,39 +24,76 @@ use utf8;
 use 5.010;
 
 use Getopt::Long;
+use Cwd;
 use Time::Piece;
+use File::Spec;
+
+# Internal variables
+my $isdir = 0;
 
 # Argument variables
-my $client = '';
+my $doclist = '';
 my $dir_source = '';
 my $dir_output = '';
 
 # Option variables
 my $type_input = "markdown";
 my $type_output = "pdf";
-my $config = "default.cfg";
+my $client= "op5";
 
 # Internal variables
-my $date = localtime->strftime("%Y%m%d");
+my $date = localtime->strftime( "%Y%m%d" );
 
-if (@ARGV < 3) {
-    die "Not enough arguments.\n";
-} elsif (@ARGV > 3) {
-    die "Too many arguments.\n";
+# ---- Subroutines Start ----
+
+# Checking path.
+sub check_path {
+    my ( $path, $create ) = @_;
+    my $isdir = 0;
+    my $doesexist = 0;
+
+    if ( !File::Spec->file_name_is_absolute( $path ) ){
+        $path = Cwd::abs_path( $path );
+    }
+
+    if ( -e $path && -f $path ) {
+        $doesexist = 1;
+        print "$path exists and is a file.\n";
+        print "Now check for access.\n";
+    } elsif ( -e $path && -d $path ) {
+        $doesexist = 1;
+        $isdir = 1;
+        print "$path exists and is a directory.\n";
+        print "Now check for access.\n";
+    } else {
+        print "$path does not exist.\n";
+    }
+
+    return $path, $doesexist, $isdir;
 }
+# ----  Subroutines End  ----
 
 GetOptions (
-	"input-type=s" => \$type_input,
-	"output-type=s" => \$type_output,
-    "config-file=s" => \$config
+	"input-type|i=s" => \$type_input,
+	"output-type|o=s" => \$type_output,
+    "client|c=s" => \$client
 );
 
-($client, $dir_source, $dir_output) = @ARGV;
+if (@ARGV != 3) {
+    die "$0 take 3 arguments.\nUSAGE: $0 <doclistfile> <sourcedir> <outputdir>\n";
+}
 
-print "$client\n";
+($doclist, $dir_source, $dir_output) = @ARGV;
+
+print "$doclist\n";
 print "$dir_source\n";
 print "$dir_output\n";
 print "$type_input\n";
 print "$type_output\n";
-print "$config\n";
+print "$client\n";
+
+( $doclist, $isdir ) = check_path( $doclist, 0 );
+( $dir_source, $isdir ) = check_path( $dir_source, 0 );
+( $dir_output, $isdir ) = check_path( $dir_output, 1 );
+
 
